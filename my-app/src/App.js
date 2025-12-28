aace92bb371a178212579ee00ecf86788864c7a1
JungHyeonKang <rkdwjdgus0103@gmail.com>
2025-12-28 13:06:19 +0900

    ADD cache yml2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload2.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction 2
+          Learn GithubAction 3
         </a>
       </header>
     </div>
